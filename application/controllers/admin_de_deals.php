2d8433e6b72e691f8503b34277bb4e1e69804c99
yangpingmin <ypm66@126.com>
2019-10-12 10:52:08 +0800

    test admin

---
+++ application/controllers/admin_de_deals.php
@@ -0,0 +1,8 @@
+<?php
+if (! defined ( 'BASEPATH' ))
+	exit ( 'No direct script access allowed' );
+class Admin_de_deals extends CI_Controller {
+	function index() {
+		echo 'test_admin_de_deals';
+	}
+}
